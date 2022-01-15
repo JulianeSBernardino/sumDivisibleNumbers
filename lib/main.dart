acca5e36649c5a20c795212802f46e6bfbef3147
Juliane Bernardino <52682074+JulianeSBernardino@users.noreply.github.com>
2022-01-14 22:09:03 -0300

    remove: unused comments

--- lib/main.dart
+++ lib/main.dart
@@ -9,19 +9,3 @@ main() {
   response.number = int.parse(input!);
   response.calcSum();
 }
-//   var input = stdin.readLineSync();
-//   print(input);
-
-//   var n = 10;
-//   for (var i = 0; i < n; i++) {
-//     if (i % 3 == 0) {
-//       print('Multiplo de 3 ' + i.toString());
-//     }
-//     if (i % 5 == 0) {
-//       print('Multiplo de 5 ' + i.toString());
-//     }
-
-//     print(i);
-//   }
-//   print("olá mundo!!");
-// }
